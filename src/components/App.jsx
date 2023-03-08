3a6333dbfe7e3ea5d4d0d92cb75639090cbf0150
AntonBabenko96 <ababenko159@gmail.com>
2023-03-08 15:41:12 +0200

    aaa

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import { ContactList } from './ContactList/ContactList';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { Box } from './Box/Box';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/operations';
+import { useDispatch, useSelector } from 'react-redux';
+import { Loader } from './Loader/Loader';
+
+import css from '../components/App.module.css';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(state => state.contacts.isLoading);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.wrapper}>
+      {isLoading && <Loader />}
+      <Box title="Phonebook">
+        <ContactForm />
+      </Box>
+      <Box title="Contacts">
+        <Filter />
+        <ContactList className={css.contacts} />
+      </Box>
     </div>
   );
 };
---
+++ src/redux/operations.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://6408805c2f01352a8a94b712.mockapi.io/';
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addNewContact = createAsyncThunk(
+  'contacts/addNewContact',
+  async (contact, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', contact);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (id, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${id}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
